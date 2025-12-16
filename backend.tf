f612ac03a3db6e6991156d2fcfd753b6e7e97940
Matajur <126118653+Matajur@users.noreply.github.com>
2025-12-17 00:58:36 +0100

    Add lesson-5-6: VPC and EKS infrastructure

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket  = "mlops-tfstate-matajur"
-    key     = "global/s3/terraform.tfstate"
-    region  = "us-east-1"
-    profile = "default"
-  }
-}
--- variables.tf
+++
@@ -1,5 +0,0 @@
-variable "bucket_name" {
-  description = "Name of the S3 bucket to store Terraform state"
-  type        = string
-  default     = "mlops-tfstate-matajur"
-}
